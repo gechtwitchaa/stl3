e1a118c077d20ff840364196e5e5b999af8ec387
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-15 19:26:55 +0100

    int main

--- ej1.cpp
+++ ej1.cpp
@@ -11,3 +11,10 @@ namespace Matematicas {
         return a - b;
     }
 }
+
+int main() {
+    int x = 20, y = 10;
+    std::cout << "Suma: " << Matematicas::suma(x, y) << std::endl;
+    std::cout << "Resta: " << Matematicas::resta(x, y) << std::endl;
+    return 0;
+}
