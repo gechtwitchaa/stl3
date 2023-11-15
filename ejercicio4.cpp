9ed148443100196ddbcd4f898c71f85af31c86de
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-15 21:26:35 +0100

    ejercicio 4 añadimos 2 subespacios con calculos y constantes (quimica y mates )

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -8,13 +8,24 @@ namespace Ciencia {
             return masa * velocidadLuz * velocidadLuz;
         }
     }
+
+    namespace Quimica {
+        const double constanteAvogadro = 6.022e23;
+    }
+
+    namespace Matematicas {
+        const double constantePi = 3.14159265359;
+    }
 }
 
 int main() {
-    double masa = 10.0;
+    double masa = 5.0;
 
     std::cout << "Energía correspondiente a una masa de " << masa << " kg: "
               << Ciencia::Fisica::calcularEnergia(masa) << " Joules" << std::endl;
+
+    std::cout << "Constante de Avogadro: " << Ciencia::Quimica::constanteAvogadro << std::endl;
+    std::cout << "Valor de pi: " << Ciencia::Matematicas::constantePi << std::endl;
 
     return 0;
 }
