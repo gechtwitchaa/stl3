ddedc559ddda618b8e9a039d851b7b81b97a20de
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-15 19:25:58 +0100

    int suma y resta

--- ej1.cpp
+++ ej1.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 namespace Matematicas {
+
+
     int suma(int a, int b) {
         return a + b;
     }
 
-    int resto(int a, int b) {
+    int resta(int a, int b) {
         return a - b;
     }
 }
