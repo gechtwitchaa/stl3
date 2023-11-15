6c21ccad3c5b7ec128f2e43b86b7dfff25834bae
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-15 19:54:10 +0100

    ej5 map

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -7,3 +7,10 @@ int main() {
 
     std::vector<int> miVector = {1, 2, 3, 4, 5};
     miVector.push_back(6);
+
+        std::map<int, std::string> miMapa;
+        miMapa[1] = "Uno";
+        miMapa[2] = "Dos";
+        miMapa[3] = "Tres";
+
+       
